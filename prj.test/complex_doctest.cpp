e80925619f7a92cc6a82739ba3c803c0b0e3a5c7
EkaterinaBykova22 <111505301+EkaterinaBykova22@users.noreply.github.com>
2024-10-31 11:18:28 +0300

    Update complex_doctest.cpp

--- prj.test/complex_doctest.cpp
+++ prj.test/complex_doctest.cpp
@@ -1,1 +1,9 @@
+#include <complex/complex.hpp>
 
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+
+TEST_CASE("[complex] - ctor") {
+  CHECK(Complex() == Complex(0.0, 0.0));
+  CHECK(Complex(2.0) == Complex(2.0, 0.0));
+}
