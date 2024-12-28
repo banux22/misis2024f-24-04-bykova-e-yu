e0fec4f0e5c51681d2987cd75f2470848ad80b06
EkaterinaBykova22 <111505301+EkaterinaBykova22@users.noreply.github.com>
2024-12-28 20:43:30 +0300

    Update arrayd.hpp

--- prj.lab/arrayd/arrayd.hpp
+++ prj.lab/arrayd/arrayd.hpp
@@ -1,1 +1,27 @@
+#ifndef ARRAYD_H
+#define ARRAYD_H
 
+#include <cstddef>
+
+class DynArr {
+ public:
+  DynArr() = default;
+  DynArr(const DynArr& rhs);
+  DynArr(DynArr&& rhs) noexcept;
+  [[nodiscard]]DynArr(const std::ptrdiff_t size);
+  ~DynArr();
+  DynArr& operator=(const DynArr& dynarr);
+  DynArr& operator=(DynArr&& rhs) noexcept;
+
+  [[nodiscard]]std::ptrdiff_t Size() const noexcept { return size_; }
+  [[nodiscard]]std::ptrdiff_t Capacity() const noexcept { return capacity_; }
+  void Resize(const std::ptrdiff_t size);
+  [[nodiscard]]float& operator[](const std::ptrdiff_t idx);
+  [[nodiscard]]const float& operator[](const std::ptrdiff_t idx) const;
+ private:
+  std::ptrdiff_t size_ = 0;
+  std::ptrdiff_t capacity_ = 0;
+  float* data_ = nullptr;
+};
+
+#endif //ARRAYD_H
