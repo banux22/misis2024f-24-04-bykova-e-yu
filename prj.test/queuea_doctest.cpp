5b279507c088888d55b09422c749e3af89cdd9d7
EkaterinaBykova22 <111505301+EkaterinaBykova22@users.noreply.github.com>
2025-01-07 00:17:47 +0300

    Update queuea_doctest.cpp

--- prj.test/queuea_doctest.cpp
+++ prj.test/queuea_doctest.cpp
@@ -1,1 +1,9 @@
+#include <queuea/queuea.hpp>
 
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+
+TEST_CASE("[queuea] - ctor") {
+  auto que_def = QueueA();
+  CHECK(que_def.IsEmpty());
+}
